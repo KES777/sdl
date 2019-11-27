7ed1187e6b8cfa0cd1e301976bbda5cd551af4fc
Nazar Kondratyuk <muhach@ukr.net>
2019-11-27 18:43:47 +0200

    Fix bag - btn_del did`nt come back itself color, after an object delete.

--- lib/Btn_del.pm
+++ lib/Btn_del.pm
@@ -6,18 +6,29 @@ use base 'Rect';
 my $START_X =  250;
 my $START_Y =    0;
 
+my $r       =  255;
+my $g       =    0;
+my $b       =    0;
+
 
 sub new {
 	my( $btn_del ) =  @_;
 
 	$btn_del  =  $btn_del->SUPER::new;
-	$btn_del->{ c } =   Color->new( 255,0,0 );
 
+	$btn_del->set_start_color;
 	$btn_del->set_start_position;
 
 	return $btn_del;
 }
 
+
+
+sub set_start_color {
+	my( $btn_del ) =  @_;
+
+	$btn_del->{ c } =   Color->new( $r, $g, $b );
+}
 
 
 sub set_start_position {
@@ -92,13 +103,15 @@ sub moving_off {
 }
 
 
+
 ## Возвращает кнопку на стартовую позицию
 sub start_position {
 	my( $btn_del ) =  @_;
 
-	$btn_del->restore_state;
+	# $btn_del->restore_state;
 	$btn_del->draw_black;
 	$btn_del->set_start_position;
+	$btn_del->set_start_color;
 }
 
 
@@ -111,13 +124,12 @@ sub store {}
 sub on_mouse_over {
 	my( $btn_del ) =  @_;
 
-
 	$btn_del->{ c }{ b } =  250;
 }
 
 
 
-## Возвращает объекту-кнопке её цвет (когда курсор с него уходит)
+## Возвращает объекту-кнопке её цвет (когда курсор с неё уходит)
 sub on_mouse_out {
 	my( $btn_del ) =  @_;
 
