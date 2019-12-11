78076a8bc56f9e1ae4b1214bd65da584b2fade3c
Eugen Konkov <kes-kes@yandex.ru>
2019-12-11 21:23:12 +0200

    Implemented ScrollBar

--- lib/Scroll_bar.pm
+++ lib/Scroll_bar.pm
@@ -1,69 +1,63 @@
+package _bar;
+
+use strict;
+use warnings;
+
+use Rect;
+
+
+use base 'Rect';
+
+
+
+sub store {}
+
+sub on_move {
+	my( $shape, $e, $h ) =  @_;
+
+	my $dy =  $e->motion_y -$h->{ event_old_y };
+	$h->{ event_old_y } =  $e->motion_y;
+	$shape->move_by( 0, $dy );
+	$shape->clip;
+
+	my $length =  $shape->{ parent }{ h } -$shape->{ h };
+	my $pos    =  $shape->{ y };
+	$shape->{ parent }{ pos } =  $pos / $length;
+	print $shape->{ parent }{ pos }, "\n";
+}
+
+
+
 package Scroll_bar;
 
 use strict;
 use warnings;
 
-
-use Scalar::Util qw(weaken);
-
-use Table;
 use Rect;
+use Color;
+
+
 use base 'Rect';
 
-my $r =  255;
-my $g =  255;
-my $b =  255;
 
 
+sub store { }
 
 sub new {
-	my( $scroll) =  shift;
+	my( $scroll, $dimension ) =  ( shift, shift );
 
-	my $rect =  $scroll->SUPER::new( @_ );
+	$scroll =  $scroll->SUPER::new( @_ );
 
-		# $rect->{ c } =   Color->new( $r, $g, $b );
 
+	my $h =  $scroll->{ h } / $dimension  *$scroll->{ h };
+	my $arm =  _bar->new( 0, 0, $scroll->{ w }, $h );
+	$arm->{ c } =  Color->new( 50, 250, 50 );
+
+	$scroll->children( $arm );
+
+
+	return $scroll;
 }
-
-
-
-sub draw {
-	my( $btn_del, $screen, $x, $y ) =  @_;
-
-	$btn_del->draw_black;
-	$btn_del->save_draw_coord;
-
-	$screen //=  AppRect::SCREEN();
-	$x //=  0;
-	$y //=  0;
-
-	$x += $btn_del->{ x };
-	$y += $btn_del->{ y };
-
-	$screen->draw_rect([
-		$x,
-		$y,
-		$btn_del->{ w },
-		$btn_del->{ h },
-	],[
-		0,0,255,255
-	]);
-
-	#circuit
-	$screen->draw_rect([
-		$x +2,
-		$y +2,
-		$btn_del->{ w }-4,
-		$btn_del->{ h }-4,
-	],[
-		$btn_del->{ c }->get()
-	]);
-
-}
-
-
-
-sub is_over_res_field{ }
 
 
 1;
