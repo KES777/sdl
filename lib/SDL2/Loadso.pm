aec42aca03bb73a2c603dbdde8c42400ce4ffe7b
Eugen Lut <misslsv@mail.ru>
2019-12-21 17:01:51 +0200

    Adding functions to the module Loadso

--- lib/SDL2/Loadso.pm
+++ lib/SDL2/Loadso.pm
@@ -4,10 +4,28 @@ use strict;
 use warnings;
 
 
+use SDL2::Stdinc;
+use SDL2::Error;
+
+my $processed;
 sub attach {
+	!$processed   or return;
+	$processed++;
 
 	my( $ffi ) =  @_;
 
+	SDL2::Stdinc::attach( $ffi );
+	SDL2::Error::attach( $ffi );
+
+	# extern DECLSPEC void *SDLCALL SDL_LoadObject(const char *sofile);
+	$ffi->attach( SDL_LoadObject => [ 'string' ] => 'opaque' );
+
+	# extern DECLSPEC void *SDLCALL SDL_LoadFunction(void *handle,
+	#                                                const char *name);
+	$ffi->attach( SDL_LoadFunction => [ 'opaque', 'string' ] => 'opaque' );
+
+	# extern DECLSPEC void SDLCALL SDL_UnloadObject(void *handle);
+	$ffi->attach( SDL_UnloadObject => [ 'opaque' ] => 'void' );
 
 }
 
