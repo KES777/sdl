3fc8226a8767b718e4a993b764f11e452cfa5c3c
Eugen Lut <misslsv@mail.ru>
2019-12-21 16:54:39 +0200

    Adding functions to modules

--- lib/SDL2/Gesture.pm
+++ lib/SDL2/Gesture.pm
@@ -3,10 +3,42 @@ package SDL2::Gesture;
 use strict;
 use warnings;
 
+use SDL2::Stdinc;
+use SDL2::Error;
+use SDL2::Video;
+use SDL2::Touch;
 
+my $processed;
 sub attach {
+	!$processed   or return;
+	$processed++;
+
 
 	my( $ffi ) =  @_;
+
+
+# ??????????????????
+	# typedef Sint64 SDL_GestureID;
+	$ffi->type( 'sint64' => 'SDL_GestureID' );
+
+
+	SDL2::Stdinc::attach( $ffi );
+	SDL2::Error::attach( $ffi );
+	SDL2::Video::attach( $ffi );
+	SDL2::Touch::attach( $ffi );
+
+
+	# extern DECLSPEC int SDLCALL SDL_RecordGesture(SDL_TouchID touchId);
+	$ffi->attach( SDL_RecordGesture => [ 'SDL_TouchID'  ] => 'int' );
+
+	# extern DECLSPEC int SDLCALL SDL_SaveAllDollarTemplates(SDL_RWops *dst);
+	$ffi->attach( SDL_SaveAllDollarTemplates => [ 'SDL_RWops_ptr'  ] => 'int' );
+
+	# extern DECLSPEC int SDLCALL SDL_SaveDollarTemplate(SDL_GestureID gestureId,SDL_RWops *dst);
+	$ffi->attach( SDL_SaveDollarTemplate => [ 'SDL_GestureID', 'SDL_RWops_ptr'  ] => 'int' );
+
+	# extern DECLSPEC int SDLCALL SDL_LoadDollarTemplates(SDL_TouchID touchId, SDL_RWops *src);
+	$ffi->attach( SDL_LoadDollarTemplates => [ 'SDL_TouchID', 'SDL_RWops_ptr' ] => 'int' );
 
 
 }
--- lib/SDL2/Touch.pm
+++ lib/SDL2/Touch.pm
@@ -4,10 +4,54 @@ use strict;
 use warnings;
 
 
+use SDL2::Stdinc;
+use SDL2::Error;
+use SDL2::Video;
+
+my $processed;
 sub attach {
+	!$processed   or return;
+	$processed++;
+
 
 	my( $ffi ) =  @_;
 
+	# typedef Sint64 SDL_TouchID;
+	# typedef Sint64 SDL_FingerID;
+	$ffi->type( 'sint64' => 'SDL_TouchID' );
+	$ffi->type( 'sint64' => 'SDL_FingerID' );
+
+	$ffi->type( 'int'    => 'SDL_TouchDeviceType' );     #enum
+	$ffi->type( 'opaque' => 'SDL_Finger_ptr' );          #struct
+
+
+# ????????????????((Uint32)-1)
+	# define SDL_TOUCH_MOUSEID ((Uint32)-1)
+	use constant SDL_TOUCH_MOUSEID   => '((Uint32)-1)';
+
+	# define SDL_MOUSE_TOUCHID ((Sint64)-1)
+	use constant SDL_MOUSE_TOUCHID   => '((Sint64)-1)';
+
+
+
+
+	SDL2::Stdinc::attach( $ffi );
+	SDL2::Error::attach( $ffi );
+	SDL2::Video::attach( $ffi );
+
+	$ffi->attach( SDL_GetNumTouchDevices => [ 'void'  ] => 'int' );
+
+	# extern DECLSPEC SDL_TouchID SDLCALL SDL_GetTouchDevice(int index);
+	$ffi->attach( SDL_GetTouchDevice => [ 'int'  ] => 'SDL_TouchID' );
+
+	# extern DECLSPEC SDL_TouchDeviceType SDLCALL SDL_GetTouchDeviceType(SDL_TouchID touchID);
+	$ffi->attach( SDL_GetTouchDeviceType => [ 'SDL_TouchID'  ] => 'SDL_TouchDeviceType' );
+
+	# extern DECLSPEC int SDLCALL SDL_GetNumTouchFingers(SDL_TouchID touchID);
+	$ffi->attach( SDL_GetNumTouchFingers => [ 'SDL_TouchID'  ] => 'int' );
+
+	# extern DECLSPEC SDL_Finger * SDLCALL SDL_GetTouchFinger(SDL_TouchID touchID, int index);
+	$ffi->attach( SDL_GetTouchFinger => [ 'SDL_TouchID', 'int'  ] => 'SDL_Finger_ptr' );
 
 }
 
