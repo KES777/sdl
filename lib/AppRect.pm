7f770a47d1033fa463fa385a7bb4e1656c0b84ef
Eugen Konkov <kes-kes@yandex.ru>
2019-12-25 21:13:55 +0200

    Implement user_event function

--- lib/AppRect.pm
+++ lib/AppRect.pm
@@ -24,6 +24,7 @@ use Cursor;
 use base 'Shape';
 
 
+my $HINT_EVENT;
 my $HINT_TIMER =  1000;
 my $APP;
 sub SCREEN { return $APP }
@@ -31,7 +32,7 @@ sub SCREEN { return $APP }
 ## Создаёт событие USEREVENT
 sub issue_hint_event {
 	my $event =  SDL::Event->new();
-	$event->type( SDL_USEREVENT );
+	$event->type( $HINT_EVENT );
 	$event->user_code( 10 );
 	$event->user_data1( 'hello event' );
 
@@ -39,6 +40,24 @@ sub issue_hint_event {
 
 	return 0;
 };
+
+
+
+# Добавляет обработчик и возвращает следующий номер для пользовательского события
+my $next_user_event_type =  0;
+sub user_event {
+	my( $handler ) =  @_;
+
+	my $user_event =  SDL_USEREVENT + $next_user_event_type++;
+	$APP->add_event_handler( sub{
+		#my( $e ) =  @_;
+		$_[0]->type == $user_event   or return;
+
+		&$handler;
+	});
+
+	return $user_event;
+}
 
 
 sub new {
@@ -79,30 +98,23 @@ sub new {
 
 	$APP->add_show_handler ( sub{ $app_rect->draw } );
 
+	$HINT_EVENT =  user_event( sub{
+		my( $e, $app ) =  @_;
 
-	$APP->add_event_handler( sub{ _on_user_event( @_, $app_rect ) } );
+		##!
+		if( my $h =  $app_rect->{ is_hint } ) {
+			$h->{ target }->on_hint( $h, $h->{ x }, $h->{ y } );
+		}
+	});
+
 	$APP->add_event_handler( sub{ _on_mouse_move( @_, $app_rect ) } );
 	$APP->add_event_handler( sub{ _is_mousedown ( @_, $app_rect ) } );
 	$APP->add_event_handler( sub{ _is_mouseup   ( @_, $app_rect ) } );
-
 
 	$APP->add_event_handler( sub{ _observer( @_, $app_rect ) } );
 
 
 	return $app_rect;
-}
-
-
-## Включает свойство hint (по событию USEREVENT )
-sub _on_user_event {
-	my( $e, $app, $app_rect ) =  @_;
-
-	$e->type == SDL_USEREVENT  &&  $e->user_code == 10   or return;
-
-	##!
-	if( my $h =  $app_rect->{ is_hint } ) {
-		$h->{ target }->on_hint( $h, $h->{ x }, $h->{ y } );
-	}
 }
 
 
