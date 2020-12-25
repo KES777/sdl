490aa1365e5710a89534c28c244bc4109670d0fd
Eugen Konkov <kes-kes@yandex.ru>
2020-12-25 18:46:48 +0200

    Attach SDL_GetError

--- lib/SDL2/Error.pm
+++ lib/SDL2/Error.pm
@@ -4,9 +4,20 @@ use strict;
 use warnings;
 
 
+my $processed;
 sub attach {
+	!$processed   or return;
+	$processed++;
 
 	my( $ffi ) =  @_;
+
+	# extern DECLSPEC int SDLCALL SDL_SetError(SDL_PRINTF_FORMAT_STRING const char *fmt, ...) SDL_PRINTF_VARARG_FUNC(1);
+	# extern DECLSPEC const char *SDLCALL SDL_GetError(void);
+	$ffi->attach( SDL_GetError => [ 'void' ] => 'string' );
+
+
+
+	# extern DECLSPEC void SDLCALL SDL_ClearError(void);
 
 
 }
